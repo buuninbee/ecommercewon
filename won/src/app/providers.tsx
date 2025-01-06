735bf56b05fab304474851732fa7b6f44427d80a
buuninbee <buuninbee@gmail.com>
2025-01-05 21:42:22 -0300

    feat: configurando o arquivo do styled-components

---
+++ won/src/app/providers.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from 'react'
+
+import GlobalStyles from '@/styles/global'
+
+const Providers = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <GlobalStyles />
+      {children}
+    </>
+  )
+}
+
+export default Providers
