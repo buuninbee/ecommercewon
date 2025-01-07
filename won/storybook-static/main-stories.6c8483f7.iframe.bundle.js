ea3d3b1e72611ca81f18f45a19b0e755f046c9ec
buuninbee <buuninbee@gmail.com>
2025-01-07 18:52:28 -0300

    configurando o story-book

---
+++ won/storybook-static/main-stories.6c8483f7.iframe.bundle.js
@@ -0,0 +1,24 @@
+"use strict";(self.webpackChunkwon=self.webpackChunkwon||[]).push([[112],{"./src/components/main/stories.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,default:()=>stories});var jsx_runtime=__webpack_require__("./node_modules/.pnpm/next@15.1.3_@babel+core@7.26.0_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/next/dist/compiled/react/jsx-runtime.js"),styled_components_browser_esm=__webpack_require__("./node_modules/.pnpm/styled-components@6.1.14_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/styled-components/dist/styled-components.browser.esm.js");let t,t1,t2,t3,t4,_=t=>t;const Wrapper=styled_components_browser_esm.Ay.main(t||(t=_`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  padding: 3rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`)),Logo=styled_components_browser_esm.Ay.img(t1||(t1=_`
+  width: 25rem;
+  margin-bottom: 2rem;
+`)),Title=styled_components_browser_esm.Ay.h1(t2||(t2=_`
+  font-size: 2.5rem;
+`)),Description=styled_components_browser_esm.Ay.h2(t3||(t3=_`
+  font-size: 2rem;
+  font-weight: 400;
+`)),Illustration=styled_components_browser_esm.Ay.img(t4||(t4=_`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
+`)),Main=({title="REact Avançado",description="typesScript, ReactJs"})=>(0,jsx_runtime.jsxs)(Wrapper,{children:[(0,jsx_runtime.jsx)(Logo,{src:"/img/logo.svg",alt:"Imagem de um àtomo e react"}),(0,jsx_runtime.jsx)(Title,{children:title}),(0,jsx_runtime.jsx)(Description,{children:description}),(0,jsx_runtime.jsx)(Illustration,{src:"/img/hero-illustration.svg",alt:"Um desenvolvedor"})]}),main=Main;Main.__docgenInfo={description:"",methods:[],displayName:"Main",props:{title:{defaultValue:{value:"'REact Avançado'",computed:!1},required:!1},description:{defaultValue:{value:"'typesScript, ReactJs'",computed:!1},required:!1}}};const stories={title:"Main",component:main,parameters:{layout:"fullscreen"}},Default={}}}]);
+//# sourceMappingURL=main-stories.6c8483f7.iframe.bundle.js.map
